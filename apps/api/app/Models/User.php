e6b119f8842310e964890add60ad3f5a1ee4ee59
dpanaz <42428281+dpanaz@users.noreply.github.com>
2026-09-19 16:54:27 -0500

    Fix staff relationship import

--- apps/api/app/Models/User.php
+++ apps/api/app/Models/User.php
@@ -8,6 +8,7 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
