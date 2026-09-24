2a63736050b2fe4ae2e33689663c9b7124b5a8e5
dpanaz <42428281+dpanaz@users.noreply.github.com>
2026-09-24 17:03:26 -0500

    Create customer welcome support conversation on signup

--- apps/api/app/Http/Controllers/Auth/RegisteredUserController.php
+++ apps/api/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Services\Support\WelcomeConversationService;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -28,7 +29,7 @@ class RegisteredUserController extends Controller
      *
      * @throws ValidationException
      */
-    public function store(Request $request): RedirectResponse
+    public function store(Request $request, WelcomeConversationService $welcome): RedirectResponse
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
@@ -43,6 +44,8 @@ class RegisteredUserController extends Controller
         ]);
 
         event(new Registered($user));
+
+        $welcome->createFor($user);
 
         Auth::login($user);
 
